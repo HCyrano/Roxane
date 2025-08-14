3791c6f341ca7db866f632ac1baf409ba2924394
bruno.causse <hcyrano@free.fr>
2025-08-14 12:55:40 +0200

    fix iteration for rougly 5000 probcut_end rougly 12,5h 1000 probcut_mid rougly 17h

--- Roxane/RXRoxane/RXEngine.cpp
+++ Roxane/RXRoxane/RXEngine.cpp
@@ -2599,7 +2599,7 @@ void RXEngine::probcut_end_data(RXHashTable* HT, RXHashTable* PV) {
     RXBBPatterns sBoard;
     RXBitBoard& board = sBoard.board;
 
-    for(int n_data = 0; n_data < 1000; ++n_data) {
+    for(int n_data = 0; n_data < 5000; ++n_data) {
         hTable->reset();
         for(int depth = 2; depth <= 25; ++depth) {
             hTable->reset();
