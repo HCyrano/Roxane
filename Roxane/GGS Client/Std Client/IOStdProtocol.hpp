333b40dddb222527acbfb9d0ae10a747fac499fb
bruno.causse <hcyrano@free.fr>
2025-12-08 15:46:48 +0100

    build-08-12-2025 15h45 introduce IOStd protocol (2)

---
+++ Roxane/GGS Client/Std Client/IOStdProtocol.hpp
@@ -0,0 +1,39 @@
+/*
+ *  IOStdProtocol.h
+ *  Roxane
+ *
+ *  Created by BrunoCausse on 08/10/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef STANDART_PROTOCOL_H
+#define STANDART_PROTOCOL_H
+
+#include "RXRoxane.hpp"
+
+class RXRoxane;
+
+class IOStdProtocol {
+	
+	RXRoxane* engine;
+	
+	volatile int running;
+	
+	void InterpretCommand(int nargs, char *args[]);
+	void SyntaxError(int nargs, char *args[]);
+	
+	//synchro std::cout
+	mutable pthread_mutex_t IOSync;
+
+public:
+	
+	IOStdProtocol(RXRoxane* engine);
+	
+	void MainLoop(void);
+	void Print(const std::string msg) const;
+
+
+};
+
+#endif
