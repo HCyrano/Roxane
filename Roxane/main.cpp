773f318d18cb5cfbdf3dd2a0ddecd1cad611e677
bruno.causse <hcyrano@free.fr>
2025-12-11 13:10:04 +0100

    build-11-12-2025 13h10 fix bug in IOStd protocol max_depth in midgame

--- Roxane/main.cpp
+++ Roxane/main.cpp
@@ -74,7 +74,7 @@ int main (int argc, char * const argv[]) {
 		}
 	}
 
-    std::string version = "build-10-12-2025 13h45";
+    std::string version = "build-11-12-2025 13h10";
     
 	std::cout << "Version Roxane " << version << std::endl;
 	std::cout << "Number of threads: " << nThreads << std::endl;
